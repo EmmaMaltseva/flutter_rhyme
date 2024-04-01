55502aec29d7b82764bef7a10d1b4bb51d39259e
Intern Emma <emmamaltseva0@gmail.com>
2024-04-01 11:11:15 +0300

    структуризировала проект, добавила папки для автороутера, установила билиотеки для автороутера

---
+++ lib/features/poems/view/poems_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PoemsScreen extends StatelessWidget {
+  const PoemsScreen({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
